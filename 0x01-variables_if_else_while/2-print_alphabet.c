3a26096c9cfcc27dbd217d6d9dd0334b40544237
AhmedAlaawally <117745506+AhmedAlaawally@users.noreply.github.com>
2022-12-09 19:59:56 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
 */
 int main(void)
 {
-	int letter = 'a';
+	int letter;
 	
-	for (; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		letter = tolower(letter);
 		putchar(letter);
